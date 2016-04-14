ee4c5602dd763d2c111582bd068f704172f54f9f
whan <han.wang.cs@outlook.com>
2016-04-14 22:19:14 +0800

    test

---
+++ src/wh/code/java/test/Test.java
@@ -0,0 +1,15 @@
+package wh.code.java.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test {
+	public static void main(String[] args){
+//		int[] arr=new int[10];
+//		ArrayList<Integer> list=new ArrayList<Integer>(Arrays.asList(arr)); 
+//		
+//		ArrayList<Integer> list=new ArrayList<Integer>(10);
+//		Integer[] arr=list.toArray(new Integer[list.size()]);
+		
+	}
+}
